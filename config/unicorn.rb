8912f764ca7ea26bf58b84a9fbe103cdf8bc33de
hiromu0124 <totohiro0124@icloud.com>
2020-01-27 13:27:14 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,16 +1,4 @@
-app_path = File.expand_path('../../', __FILE__)
-
-worker_processes 1
-
-working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
-listen "#{app_path}/tmp/sockets/unicorn.sock"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
-
- ↓↓↓↓↓↓↓ 以下のように変更 ↓↓↓↓↓↓
-
-# ../が一つ増えている
+#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
@@ -22,6 +10,13 @@ listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) && GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+run_once = true
 
 before_fork do |server, worker|
   defined?(ActiveRecord::Base) &&
