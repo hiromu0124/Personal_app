8afc835cc6bb7b4c31d5fae58a1d440b82294f1b
hiromu0124 <totohiro0124@icloud.com>
2020-01-28 14:51:13 +0900

    追加

--- app/assets/javascripts/content.js
+++ app/assets/javascripts/content.js
@@ -1,35 +1,25 @@
-$(function(){
-  function buildHTML(answer){
-    var html =`<div class="answers">
-    <div class="answer__user">
-    <a class="main__menu__contents__content__user__link" href="/users/1">${answer.user.name}</a>
-    </div>
-    <div class="answer">
-    <div class="answer__created_at">
-    ${answer.created_at}
-    </div>
-    <div class="answer__text">
-    <p>${answer.text}</p>
-    </div>
-    </div>
-    </div>
-              `
-  }
-  $("#answer_text").on("submit", function(e){
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url =$(this).attr("action");
-    $.afax({
-      url: url,
-      type: 'POST',
-      data: formData,
-      dataType: 'json'
-    })
-    .done(function(answer){
-      var html = buildHTML(answer)
-      $('.answers').append(html);
-      $('.answer-form__main__btn').prop('disabled', false);
-      $(".answer-form__main__form")[0].reset();
-    })
-  });
-});
+// $(function(){
+//   // setInterval(function(){
+//   //   var time=new Date();
+//   //   document.getElementById("time").innerHTML=time.toLocaleString();
+    
+//   // })
+//   $(".like").on("submit",function(e){
+//     console.log("ok")
+//     e.preventDefault();
+//     var formData = new FormData(this);
+//     $.ajax({
+//       url: "content/create_like",  
+//       type: 'POST',  
+//       data: formData , 
+//       dataType: 'json',
+//     });
+//     done(function(){
+//       $('.show-likes').append(`<a class="heart_btn" rel="nofollow" data-method="delete" href="/contents/1/content_likes/1"><div class="heart_sub"></div>
+//       </a>`);
+//     })
+//     .fail(function() {
+//       alert("メッセージ送信に失敗しました");
+//     });
+//   });
+// });
--- app/views/contents/create_answer.json.jbuilder
+++ app/views/contents/create_answer.json.jbuilder
@@ -1,3 +1,2 @@
-json.user_name @answer.user.name
-json.created_at @answer.created_at.strftime("%Y-%m-%d %H:%M")
-json.text markdown(@answer.text).html_safe
+json.user_id current_user.id
+json.content_id @content.id
